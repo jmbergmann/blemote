2478e15229427631e6c2230e2939de27c7e0eec9
Johannes Bergmann <community@johannes-bergmann.de>
2017-10-07 15:28:45 +1300

    Top button reads hall sensors and battery voltage, bottom button cycles LEDs

--- src/main.c
+++ src/main.c
@@ -26,71 +26,55 @@ int main(void)
     hall_init();
     leds_init();
 
-    hall_set_sleep(HALL_1, false);
-    hall_set_sleep(HALL_3, false);
-
     printf("App up and running.\n\r");
 
-    int i = 0;
     while (true) {
-        char ch1 = buttons_is_top_pressed() ? '1' : '0';
-        char ch2 = buttons_is_bottom_pressed() ? '1' : '0';
-        printf("%c %c   %d\n\r", ch1, ch2, i++);
+        if (buttons_is_top_pressed()) {
+            for (int i = 0; i < 1000 && !buttons_is_bottom_pressed(); ++i) {
+                hall_set_sleep(HALL_1, false);
+                hall_set_sleep(HALL_3, false);
 
-        uint16_t hall1 = hall_sample(HALL_1);
-        uint16_t hall3 = hall_sample(HALL_3);
-        printf("%d %d\n\r", (int)hall1, (int)hall3);
+                uint16_t hall1 = hall_sample(HALL_1);
+                uint16_t hall3 = hall_sample(HALL_3);
+                float voltage = gauge_read_voltage();
+                printf("H1: %d H3: %d BAT: %.3fV\n\r", (int)hall1, (int)hall3, voltage);
 
-        nrf_delay_ms(100);
+                nrf_delay_ms(100);
+            }
+        }
 
-        // {{
-        // uint8_t ch;
-        // bool ok = twi_read(0x60, &ch, 1);
-        // printf("TWI read %x %s\n\r", (int)0x60, ok ? "OK" : "ERR");
-        // }}
+        if (buttons_is_bottom_pressed()) {
+            for (int i = 0; i < 5 && !buttons_is_top_pressed(); ++i) {
+                int delay = 50;
+                leds_set_all(LED_BAT_BOTTOM_R); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_BOTTOM_Y1); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_BOTTOM_Y2); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_BOTTOM_G1); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_BOTTOM_G2); nrf_delay_ms(delay);
 
-        float voltage = gauge_read_voltage();
-        printf("Voltage: %.3f\n\r", voltage);
+                leds_set_all(LED_BAT_TOP_R); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_TOP_Y1); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_TOP_Y2); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_TOP_G1); nrf_delay_ms(delay);
+                leds_set_all(LED_BAT_TOP_G2); nrf_delay_ms(delay);
 
-        float b = gauge_read_thermistor_b();
-        printf("Thermistor B: %d\n\r", (int)b);
+                leds_set_all(LED_USB_Y); nrf_delay_ms(delay);
+                leds_set_all(LED_USB_G); nrf_delay_ms(delay);
 
-        int delay = 50;
-        leds_set_all(LED_BAT_BOTTOM_R); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_BOTTOM_Y1); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_BOTTOM_Y2); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_BOTTOM_G1); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_BOTTOM_G2); nrf_delay_ms(delay);
-
-        leds_set_all(LED_BAT_TOP_R); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_TOP_Y1); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_TOP_Y2); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_TOP_G1); nrf_delay_ms(delay);
-        leds_set_all(LED_BAT_TOP_G2); nrf_delay_ms(delay);
-
-        leds_set_all(LED_USB_Y); nrf_delay_ms(delay);
-        leds_set_all(LED_USB_G); nrf_delay_ms(delay);
-
-        leds_set_all(LED_SEG_DOT_BOTTOM); nrf_delay_ms(delay);
-        leds_set_all(LED_SEG_0); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_1); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_2); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_3); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_4); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_5); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_6); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_7); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_8); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_9); nrf_delay_ms(delay * 3);
-        leds_set_all(LED_SEG_DOT_TOP); nrf_delay_ms(delay);
+                leds_set_all(LED_SEG_DOT_BOTTOM); nrf_delay_ms(delay);
+                leds_set_all(LED_SEG_0); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_1); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_2); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_3); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_4); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_5); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_6); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_7); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_8); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_9); nrf_delay_ms(delay * 3);
+                leds_set_all(LED_SEG_DOT_TOP); nrf_delay_ms(delay);
+                leds_set_all(LED_NONE); nrf_delay_ms(delay);
+            }
+        }
     }
-
-    // nrf_gpio_cfg_output(H1_SLP);
-
-    // while (true) {
-    //     nrf_gpio_pin_write(H1_SLP, 1);
-    //     nrf_delay_ms(500);
-    //     nrf_gpio_pin_write(H1_SLP, 0);
-    //     nrf_delay_ms(500);
-    // }
 }
